10dc807486bf59eeb0f9b369ed10a066a4cf2ba6
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2017-05-07 00:05:11 +0900

    Problem: Crash Reporting

--- src/index.js
+++ src/index.js
@@ -7,12 +7,43 @@ const store = createStore(todoApp);
 // Log the initial state
 console.log(store.getState());
 
-let next = store.dispatch;
-store.dispatch = function dispatchAndLog(action) {
-  console.log('dispatching', action);
-  let result = next(action);
-  console.log('next state', store.getState());
-  return result;
+function patchStoreToAddLogging(store) {
+  let next = store.dispatch;
+  store.dispatch = function dispatchAndLog(action) {
+    console.log('dispatching', action);
+    let result = next(action);
+    console.log('next state', store.getState());
+    return result;
+  }
 }
+
+// Dummy
+const Raven = {
+  captureException: function(err, optional) {
+    console.error(err);
+    console.log(optional);
+  },
+};
+
+function patchStoreToAddCrashReporting(store) {
+  let next = store.dispatch;
+  store.dispatch = function dispatchAndReportErrors(action) {
+    try {
+      return next(action);
+    } catch (err) {
+      console.error('Caught an exception!', err);
+      Raven.captureException(err, {
+        extra: {
+          action,
+          state: store.getState(),
+        },
+      });
+      throw err;
+    }
+  }
+}
+
+patchStoreToAddLogging(store);
+patchStoreToAddCrashReporting(store);
 
 store.dispatch(addTodo('Use Redux'));
